6bc173e8b59d1efffb93dc6ecdfa4df6cf33cebf
Chase Callahan <ctcallahan2@crimson.ua.edu>
2023-12-17 21:01:26 -0600

    introduced monads

---
+++ haskell/drunken-monad.hs
@@ -0,0 +1,19 @@
+module Main where
+
+    data Position t = Position t deriving (Show)
+
+    stagger (Position d) = Position (d + 2)
+    crawl (Position d) = Position (d + 1)
+
+    rtn x = x
+    x >>== f = f x
+
+    treasureMap pos = pos >>==
+                    stagger >>==
+                    stagger >>==
+                    crawl >>==
+                    rtn
+
+    main :: IO()
+    main = do 
+        putStrLn ""
